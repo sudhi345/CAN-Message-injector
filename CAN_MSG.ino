b3762f77d70bb26e01cae643595776b8a257c29f
Sudheendra Hegde <46527328+sudheendra-hegde@users.noreply.github.com>
2019-03-07 14:39:25 +0530

    Update CAN_MSG.ino

--- CAN_MSG.ino
+++ CAN_MSG.ino
@@ -1,23 +1,27 @@
-#include <SPI.h>
-#include <mcp2515.h>
+#include <SPI.h>       //header file for SPI communication 
+#include <mcp2515.h>   //header file containing predifined functions for mcp2515 CAN module
 
-struct can_frame canMsg1;
+struct can_frame canMsg1;   //instance of structureto store a message 
 struct can_frame canMsg2;
-MCP2515 mcp2515(10);
+MCP2515 mcp2515(10);      //create an instance to address the module connected at D10
 
+//initialize pin numbers to variables
 int IMD=2;
 int BSPD=4;
 int AMS=6;
 int SW=8;
+
 boolean flag=0;
 boolean check=0;
 
 void setup() {
+  //set the initialized pins as input pins
 pinMode(IMD,INPUT);
 pinMode(BSPD,INPUT);
 pinMode(AMS,INPUT);
 pinMode(SW,INPUT);
 
+  //initialize message 2
   canMsg2.can_id  = 0x007;
   canMsg2.can_dlc = 8;
   canMsg2.data[0] = 0xEE;
@@ -29,11 +33,11 @@ pinMode(SW,INPUT);
   canMsg2.data[6] = 0x00;
   canMsg2.data[7] = 0xEE;
 
-Serial.begin(9600);
-SPI.begin();
+Serial.begin(9600);  //start serial communication (over USB) 
+SPI.begin();         //start SPI communication
  
-  mcp2515.reset();
-  mcp2515.setBitrate(CAN_250KBPS);
+  mcp2515.reset();    //reset the CAN module
+  mcp2515.setBitrate(CAN_250KBPS);    //set the bit rate of CAN bus
   mcp2515.setNormalMode();
 }
 
